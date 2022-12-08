8f745e3614796d50001c468f9a43f95b1aea27d0
Chandrasura25 <abdulganiy1624demilade@gmail.com>
2022-12-08 14:48:17 +0100

    last next class of the year 2022

--- pages/goods/[id].js
+++ pages/goods/[id].js
@@ -1,28 +1,29 @@
 import React from 'react'
 
 const SingleGood = ({ good }) => {
-    // console.log(good);
     return (
-        <div>SingleGood</div>
+        <div>
+        <h3>{good.name}</h3>
+        <p>{good.price}</p>
+        </div>
     )
 }
 
 export default SingleGood
 
 export async function getStaticPaths() {
-    // return {
-    //     paths: [
-    //         { params: { id: '1' } },
-    //         { params: { id: '2' } },
-    //         { params: { id: '3' } },
-    //     ],
-    //     fallback: false
-    // }
+    return {
+        paths: [
+            { params: { id: '1' } },
+            { params: { id: '2' } },
+            { params: { id: '3' } },
+        ],
+        fallback: false
+    }
 }
 export async function getStaticProps(context) {
     let response = await fetch(`http:localhost:4900/products/${context.params.id}`)
     let data = await response.json();
-    console.log(data);
 
     return {
         props: { good: data.singleProduct }
