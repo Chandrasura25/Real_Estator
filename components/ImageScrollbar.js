5390d510dbd443e5f32e140e90debcbfa8893a90
Chandrasura25 <abdulganiy1624demilade@gmail.com>
2022-12-24 10:15:01 +0100

    done

--- components/ImageScrollbar.js
+++ components/ImageScrollbar.js
@@ -7,7 +7,7 @@ const LeftArrow = () => {
     const { scrollPrev } = useContext(VisibilityContext)
     return (
         <Flex justifyContent={'center'} alignItems='center' marginRight={1}>
-            <Icon as={FaArrowAltCircleLeft} onClick={()=>scrollPrev()} cursor='pointer' fontSize={'2xl'} />
+            <Icon as={FaArrowAltCircleLeft} onClick={() => scrollPrev()} cursor='pointer' fontSize={'2xl'} />
         </Flex>
     )
 }
@@ -15,7 +15,7 @@ const RightArrow = () => {
     const { scrollNext } = useContext(VisibilityContext)
     return (
         <Flex justifyContent={'center'} alignItems='center' marginRight={1}>
-            <Icon as={FaArrowAltCircleRight} onClick={()=>scrollNext( )} cursor='pointer' fontSize={'2xl'} />
+            <Icon as={FaArrowAltCircleRight} onClick={() => scrollNext()} cursor='pointer' fontSize={'2xl'} />
         </Flex>
     )
 }
@@ -24,7 +24,7 @@ const ImageScrollbar = ({ data }) => {
         <ScrollMenu LeftArrow={LeftArrow} RightArrow={RightArrow} style={{ overflow: 'hidden' }}>
             {data.map((item) => (
                 <Box key={item.id} width='910px' itemId={item.id} overflow='hidden' p='1'>
-                    <Image placeholder='blur' blurDataURL={item.url} src={item.url} width={1000} height={500} alt='property' sizes='(max-width:500px) 100px,(max-width):1023px 400px, 1000px'/>
+                    <Image height={500} placeholder='blur' blurDataURL={item.url} src={item.url} width={1000} alt='property' sizes='(max-width:500px) 100px,(max-width):1023px 400px, 1000px' />
                 </Box>
             ))}
         </ScrollMenu>
